59a56348ac681aebd7c175563208b14e3283cfe0
Daniel Heery <danieljheery@gmail.com>
2021-06-05 16:43:12 +0100

    New Notes

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$249.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
